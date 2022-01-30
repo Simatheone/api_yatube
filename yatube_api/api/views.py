733ac9b0e781a6718d41e8b0e74530cb3933e99e
simatheone <samone07@mail.ru>
2022-01-30 19:19:45 +0300

    Добавил CommentViewSet для обработки запросов CRUD запросов.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,48 +1,49 @@
 from django.core.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+from rest_framework import permissions
 from rest_framework import viewsets
-from rest_framework import status
-from rest_framework.response import Response
 
 from posts.models import Group, Post, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_class = (permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_class = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_class = (permissions.IsAuthenticatedOrReadOnly,
+                        IsOwnerOrReadOnly)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        queryset = Comment.objects.filter(post_id=post_id)
+        return queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
             raise PermissionDenied()
-        super(PostViewSet, self).perform_update(serializer)
+        super(CommentViewSet, self).perform_update(serializer)
 
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
             raise PermissionDenied()
-        super(PostViewSet, self).perform_destroy(instance)
-
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    pass
-
-
-# api/v1/api-token-auth/ (POST): передаём логин и пароль, получаем токен.
-# api/v1/posts/ (GET, POST): получаем список всех постов или создаём новый пост.
-# api/v1/posts/{post_id}/ (GET, PUT, PATCH, DELETE): получаем, 
-# редактируем или удаляем пост по id.
-# api/v1/groups/ (GET): получаем список всех групп.
-# api/v1/groups/{group_id}/ (GET): получаем информацию о группе по id.
-
-# api/v1/posts/{post_id}/comments/ (GET, POST): получаем список 
-# всех комментариев поста с id=post_id или создаём новый, указав id поста, 
-# который хотим прокомментировать.
-
-# api/v1/posts/{post_id}/comments/{comment_id}/ (GET, PUT, PATCH, DELETE): 
-# получаем, редактируем или удаляем комментарий по id у поста с id=post_id.
+        instance.delete() 

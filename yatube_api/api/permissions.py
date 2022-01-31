449a98bbb5a04baf3150b3b7b438b6ff7e717e4f
simatheone <samone07@mail.ru>
2022-01-31 19:42:30 +0300

    Финальный проект спринт 8

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -8,4 +8,4 @@ class IsAuthorOrReadOnly(BasePermission):
     и какой метод запроса к эндпоинту.
     """
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user and request.method in SAFE_METHODS
+        return obj.author == request.user or request.method in SAFE_METHODS
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -14,6 +14,6 @@ router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    )
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
+    path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/', include(router_v1.urls)),
 ]
